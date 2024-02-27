97d50bfd44b25b2facfcf4bd19735a7aeb00d392
jasperzhong <izhongyuchen@gmail.com>
2024-02-27 11:41:32 +0800

    update

--- distributed_train.sh
+++ distributed_train.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
-NNODES=4
+NNODES=2
 NUM_PROC=4
 MASTER_IP=10.28.1.29
 MASTER_PORT=11234
 export NCCL_SOCKET_IFNAME=enp225s0
+
+# get the IP address of the current node
+IP=`hostname -I | awk '{print $1}'`
+IS_MASTER=$([ "$IP" = "$MASTER_IP" ] && echo "is master")
 
 OMP_NUM_THREADS=20 torchrun \
     --nnodes=$NNODES \
@@ -11,6 +15,7 @@ OMP_NUM_THREADS=20 torchrun \
     --rdzv_id=1234 \
     --rdzv_backend=c10d \
     --rdzv_endpoint=$MASTER_IP:$MASTER_PORT \
+    --is_host=$IS_MASTER \
     train.py "$@" 
     > train_imagenet.log 2>&1 
 
