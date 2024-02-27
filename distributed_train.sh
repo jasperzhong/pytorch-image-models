39ea00edfd528bacaf6663e24e6d44e95323a149
jasperzhong <izhongyuchen@gmail.com>
2024-02-27 10:53:14 +0800

    update

--- distributed_train.sh
+++ distributed_train.sh
@@ -4,12 +4,10 @@ NUM_PROC=4
 MASTER_IP=10.28.1.29
 MASTER_PORT=11234
 
-shift
 OMP_NUM_THREADS=20 torchrun \
     --nnodes=$NNODES \
     --nproc_per_node=$NUM_PROC \
     --rdzv_id=1234 \
     --rdzv_backend=c10d \
-    train.py "$@" \
-    > train_imagenet.log 2>&1
+    train.py "$@" 
 
